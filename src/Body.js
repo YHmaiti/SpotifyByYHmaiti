c1602778d1af3b11c2a27588d0f974d86ce64ddb
Yohan Hmaiti <88591695+YHmaiti@users.noreply.github.com>
2021-12-18 18:30:33 -0500

    Update Body.js

--- src/Body.js
+++ src/Body.js
@@ -9,49 +9,20 @@ import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Body({ spotify }) {
     const [{ discover_weekly }, dispatch] = useStateValue();
-
     const playPlaylist = (id) => {
-        spotify
-            .play({
-                context_uri: `spotify:playlist:37i9dQZEVXcJZyENOWUFo7`,
-            })
-            .then((res) => {
-                spotify.getMyCurrentPlayingTrack().then((r) => {
-                    dispatch({
+        spotify.play({ context_uri: `spotify:playlist:37i9dQZEVXcJZyENOWUFo7`, }).then((res) => {spotify.getMyCurrentPlayingTrack().then((r) => {dispatch({
                         type: "SET_ITEM",
                         item: r.item,
-                    });
-                    dispatch({
-                        type: "SET_PLAYING",
-                        playing: true,
-                    });
-                });
-            });
+                        }); dispatch({type: "SET_PLAYING",playing: true,});});});
     };
-
-    const playSong = (id) => {
-        spotify
-            .play({
-                uris: [`spotify:track:${id}`],
-            })
-            .then((res) => {
-                spotify.getMyCurrentPlayingTrack().then((r) => {
-                    dispatch({
+    const playSong = (id) => {spotify.play({uris: [`spotify:track:${id}`],}).then((res) => {spotify.getMyCurrentPlayingTrack().then((r) => {dispatch({
                         type: "SET_ITEM",
                         item: r.item,
-                    });
-                    dispatch({
-                        type: "SET_PLAYING",
-                        playing: true,
-                    });
-                });
-            });
+                        }); dispatch({ type: "SET_PLAYING",playing: true,});});});
     };
-
     return (
         <div className="body">
             <Header spotify={spotify} />
-
             <div className="body__info">
                 <img src={discover_weekly?.images[0].url} alt="" />
                 <div className="body__infoText">
@@ -60,23 +31,15 @@ function Body({ spotify }) {
                     <p>{discover_weekly?.description}</p>
                 </div>
             </div>
-
             <div className="body__songs">
                 <div className="body__icons">
-                    <PlayCircleFilledIcon
-                        className="body__shuffle"
-                        onClick={playPlaylist}
-                    />
+                    <PlayCircleFilledIcon className="body__shuffle" onClick={playPlaylist}/>
                     <FavoriteIcon fontSize="large" />
                     <MoreHorizIcon />
                 </div>
-
-                {discover_weekly?.tracks.items.map((item) => (
-                    <SongRow playSong={playSong} track={item.track} />
-                ))}
+                {discover_weekly?.tracks.items.map((item) => (<SongRow playSong={playSong} track={item.track} /> ))}
             </div>
         </div>
     );
 }
-
 export default Body;

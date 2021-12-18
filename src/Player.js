35c23a28242027148c1b9021f1f02ea4b15f08d8
Yohan Hmaiti <88591695+YHmaiti@users.noreply.github.com>
2021-12-18 18:44:34 -0500

    Update Player.js

--- src/Player.js
+++ src/Player.js
@@ -1,19 +1,10 @@
-import React from "react";
-import Footer from "./Footer";
 import "./Player.css";
 import Sidebar from "./Sidebar";
 import Body from "./Body";
+import React from "react";
+import Footer from "./Footer";
 
 function Player({ spotify }) {
-    return (
-        <div className="player">
-            <div className="player__body">
-                <Sidebar />
-                <Body spotify={spotify} />
-            </div>
-            <Footer spotify={spotify} />
-        </div>
-    );
+    return (<div className="player"><div className="player__body"><Sidebar /><Body spotify={spotify} /></div> <Footer spotify={spotify} /> </div>);
 }
-
 export default Player;
